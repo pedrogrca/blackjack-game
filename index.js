f98c45d95dd2f4baf2ea5d1897ce13e5a23e57ef
Pedro Henrique Graça <68236129+pedrogrca@users.noreply.github.com>
2025-04-08 17:04:55 -0300

    Added button dynamics

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ let message = undefined;
 let sumEl = document.getElementById("sum-el");
 let messageEl = document.getElementById("message-el");
 let cardsEl = document.getElementById("cards-el");
+let newCardButton = document.getElementById("newCard");
+let startGameButton = document.getElementById("startGame");
+let newGameMessage = document.getElementById("newGameMessage");
+let newGameButton = document.getElementById("newGameButton");
+
+
+window.onload = newCardButton.style.display = "none";
+window.onload = newGameButton.style.display = "none";
+
 
 function startGame() {
     isAlive = true;
@@ -15,30 +24,48 @@ function startGame() {
     let secondCard = getRandomCard();
     cards = [firstCard, secondCard];
     sum = firstCard + secondCard;
+    startGameButton.style.display = "none";
     renderGame();
 }
 
 function renderGame() {
     cardsEl.textContent = "Cartas: ";
+    newCardButton.style.display = "block";
+    newGameButton.style.display = "none";
+
 
     for(i = 0; i < cards.length; i++){
         cardsEl.textContent += cards[i] + " "
     }
 
     sumEl.textContent = "Soma: " + sum;
+
     if (sum <= 20) {
     message = "Gostaria de puxar uma nova carta?";
     } else if (sum === 21) {
     message = "Parabéns, você ganhou!";
     hasBlackJack = true;
+    newGameMessage.textContent = "Gostaria de jogar novamente?";
+    newGameButton.style.display = "block";
+    newGameButton.textContent = "JOGAR NOVAMENTE"
+    newCardButton.style.display = "none";
     } else {
     message = "Você Perdeu!";
     isAlive = false;
+    newGameMessage.textContent = "Gostaria de jogar novamente?";
+    newGameButton.style.display = "block";
+    newGameButton.textContent = "JOGAR NOVAMENTE"
+    newCardButton.style.display = "none";
     }
 
 
 messageEl.textContent = message;
 
+}
+
+function playAgain() {
+    newGameMessage.textContent = "";
+    startGame();
 }
 
 
