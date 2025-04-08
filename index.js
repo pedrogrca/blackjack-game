1db4623f83919a9de4b742026ea89881814b865e
Pedro Henrique Graça <68236129+pedrogrca@users.noreply.github.com>
2025-04-08 12:56:08 -0300

    Added Buttons and New Functions to the Game

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-let firstCard = 10;
+let firstCard = 8;
 let secondCard = 10;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = undefined;
 
-if (sum <= 20) {
+let sumEl = document.getElementById("sum-el")
+let messageEl = document.getElementById("message-el")
+let cardsEl = document.getElementById("cards-el")
+
+function startGame() {
+    if (sum <= 20) {
     message = "Gostaria de puxar uma nova carta?";
-} else if (sum === 21) {
+    } else if (sum === 21) {
     message = "Parabéns, você ganhou!";
     hasBlackJack = true;
-} else {
+    } else {
     message = "Você Perdeu!";
     isAlive = false;
+    }
+
+cardsEl.textContent = "Cartas: " + firstCard + " " + secondCard
+sumEl.textContent = "Soma: " + sum;
+messageEl.textContent = message;
+
 }
 
+function newCard() {
+    console.log("Nova carta puxada!")
+}
 
-console.log(message);
