9c166045008953ac5127d6fd7c80fc555ed4dae1
Pedro Henrique Graça <68236129+pedrogrca@users.noreply.github.com>
2025-04-08 15:35:26 -0300

    Added enhanced logic and style to the game.

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-let firstCard = 8;
-let secondCard = 10;
-let sum = firstCard + secondCard;
+let cards = [];
+let isAlive = false;
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
 let message = undefined;
 
-let sumEl = document.getElementById("sum-el")
-let messageEl = document.getElementById("message-el")
-let cardsEl = document.getElementById("cards-el")
+let sumEl = document.getElementById("sum-el");
+let messageEl = document.getElementById("message-el");
+let cardsEl = document.getElementById("cards-el");
 
 function startGame() {
+    isAlive = true;
+    hasBlackJack = false;
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard, secondCard];
+    sum = firstCard + secondCard;
+    renderGame();
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cartas: ";
+
+    for(i = 0; i < cards.length; i++){
+        cardsEl.textContent += cards[i] + " "
+    }
+
+    sumEl.textContent = "Soma: " + sum;
     if (sum <= 20) {
     message = "Gostaria de puxar uma nova carta?";
     } else if (sum === 21) {
@@ -20,13 +36,33 @@ function startGame() {
     isAlive = false;
     }
 
-cardsEl.textContent = "Cartas: " + firstCard + " " + secondCard
-sumEl.textContent = "Soma: " + sum;
+
 messageEl.textContent = message;
 
 }
 
-function newCard() {
-    console.log("Nova carta puxada!")
+
+function getRandomCard(){
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+
+    if (randomNumber === 1) {
+        return 11;
+    } else if (randomNumber > 10){
+        return 10;
+    } else {
+        return randomNumber;
+    }
+        
+
 }
 
+
+
+function newCard() {
+    if (isAlive === true && hasBlackJack === false){
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    renderGame();
+    }
+}
