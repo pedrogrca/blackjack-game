3f0b5271bb8fd52382d664ab959094ec85536258
Pedro Henrique Graça <68236129+pedrogrca@users.noreply.github.com>
2025-04-14 10:16:33 -0300

    Fixed bugs and avoided code repetition.

--- indessx.js
+++ indessx.js
@@ -4,24 +4,24 @@ let isAlive = false;
 let sum = 0;
 let hasBlackJack = false;
 let message = undefined;
-// let player = {
-//     name: "Pedro",
-//     chips: 150
-// }
+let player = {
+    name: "Pedro",
+    chips: 150
+}
 
 
-const sumEl = document.getElementById("sum-el");
-const messageEl = document.getElementById("message-el");
-const cardsEl = document.getElementById("cards-el");
-const newCardButton = document.getElementById("newCard");
-const startGameButton = document.getElementById("startGame");
-const newGameMessage = document.getElementById("newGameMessage");
-const newGameButton = document.getElementById("newGameButton");
-const standButton = document.getElementById("standButton");
-const dealer = document.getElementById("dealer");
-const dealerCards = document.getElementById("dealerCards");
-const dealerCardsSum = document.getElementById("dealerCardsSum")
-const playerEl = document.getElementById("player");
+let sumEl = document.getElementById("sum-el");
+let messageEl = document.getElementById("message-el");
+let cardsEl = document.getElementById("cards-el");
+let newCardButton = document.getElementById("newCard");
+let startGameButton = document.getElementById("startGame");
+let newGameMessage = document.getElementById("newGameMessage");
+let newGameButton = document.getElementById("newGameButton");
+let standButton = document.getElementById("standButton");
+let dealer = document.getElementById("dealer");
+let dealerCards = document.getElementById("dealerCards");
+let dealerCardsSum = document.getElementById("dealerCardsSum")
+let playerEl = document.getElementById("player");
 
 
 
@@ -53,14 +53,6 @@ function startGame() {
     dealerGame();
 }
 
-function newGameOption() {
-    newGameMessage.textContent = "Gostaria de jogar novamente?";
-    newGameButton.style.display = "block";
-    newGameButton.textContent = "JOGAR NOVAMENTE"
-    newCardButton.style.display = "none";
-    standButton.style.display = "none";
-}
-
 function renderGame() {
     cardsEl.textContent = "Cartas: ";
     newCardButton.style.display = "block";
@@ -81,15 +73,28 @@ function renderGame() {
     } else if (sum === 21) {
     message = "Parabéns, você conseguiu Blackjack!";
     hasBlackJack = true;
-    newGameOption()
+    newGameMessage.textContent = "Gostaria de jogar novamente?";
+    newGameButton.style.display = "block";
+    newGameButton.textContent = "JOGAR NOVAMENTE"
+    newCardButton.style.display = "none";
+    standButton.style.display = "none";
     } else if (sum > 21) {
     message = "Você estourou, perdeu!";
     isAlive = false;
-    newGameOption()
+    newGameMessage.textContent = "Gostaria de jogar novamente?";
+    newGameButton.style.display = "block";
+    newGameButton.textContent = "JOGAR NOVAMENTE"
+    newCardButton.style.display = "none";
+    standButton.style.display = "none";
     } else {
     message = "Você ganhou, parabéns!";
     isAlive = true;
-    newGameOption()
+    newGameMessage.textContent = "Gostaria de jogar novamente?";
+    newGameButton.style.display = "block";
+    newGameButton.textContent = "JOGAR NOVAMENTE"
+    newCardButton.style.display = "none";
+    standButton.style.display = "none";
+
     }
 
 
@@ -138,9 +143,13 @@ function newCard() {
 
 
 function stand() {
+    let dealerSum = cardsDealer.reduce((a, b) => a + b, 0);
     newCardButton.style.display = "none";
     standButton.style.display = "none";
+    let secondDealerCard = getRandomCard();
+    cardsDealer.push(secondDealerCard)
     dealerCards.textContent = "Cartas: " + cardsDealer + " ";
+
     newDealerCard();
     checkWinner();
 }
@@ -168,24 +177,45 @@ function newDealerCard() {
 
 function checkWinner() {
     let dealerSum = cardsDealer.reduce((a, b) => a + b, 0);
-    dealerCards.textContent = `Cartas: ${cardsDealer}`;
+    console.log(`Soma Player: ${sum}`)
+    console.log(`Soma Dealer: ${dealerSum}`)
     dealerCardsSum.textContent = dealerSum;
     if (dealerSum === 21) {
         messageEl.textContent = "A banca conseguiu BlackJack, você Perdeu!"
-        newGameOption()
+        newGameMessage.textContent = "Gostaria de jogar novamente?";
+        newGameButton.style.display = "block";
+        newGameButton.textContent = "JOGAR NOVAMENTE"
+        newCardButton.style.display = "none";
+        standButton.style.display = "none";
     } else if (dealerSum > 21) {
         messageEl.textContent = "A banca estourou, você venceu!"
-        newGameOption()
+        newGameMessage.textContent = "Gostaria de jogar novamente?";
+        newGameButton.style.display = "block";
+        newGameButton.textContent = "JOGAR NOVAMENTE"
+        newCardButton.style.display = "none";
+        standButton.style.display = "none";
     } else if (dealerSum > sum && dealerSum < 21 && sum < 21) {
         messageEl.textContent = "Você perdeu!"
-        newGameOption()
+        newGameMessage.textContent = "Gostaria de jogar novamente?";
+        newGameButton.style.display = "block";
+        newGameButton.textContent = "JOGAR NOVAMENTE"
+        newCardButton.style.display = "none";
+        standButton.style.display = "none";
     }
     else if (dealerSum === sum) {
         messageEl.textContent = "Empate!"
-        newGameOption()
+        newGameMessage.textContent = "Gostaria de jogar novamente?";
+        newGameButton.style.display = "block";
+        newGameButton.textContent = "JOGAR NOVAMENTE"
+        newCardButton.style.display = "none";
+        standButton.style.display = "none";
      
     } else if (dealerSum < 21 && dealerSum < sum) {
         messageEl.textContent = "Você Venceu!"
-        newGameOption()
+        newGameMessage.textContent = "Gostaria de jogar novamente?";
+        newGameButton.style.display = "block";
+        newGameButton.textContent = "JOGAR NOVAMENTE"
+        newCardButton.style.display = "none";
+        standButton.style.display = "none";
     }
 }
